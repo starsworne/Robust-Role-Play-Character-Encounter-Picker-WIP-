ec4f5fae2441838d30c3ae3a04f780aa2d6aa7bb
♯ Windblade <cambria.shepard@gmail.com>
2021-11-24 18:53:22 -0800

    Add project files.

---
+++ Robust Role Play Character Encounter Picker/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Robust_Role_Play_Character_Encounter_Picker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Robust Role Play Character Encounter Picker/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robust_Role_Play_Character_Encounter_Picker
+{
+    class Character
+    {
+        public string Name;
+        public string RolePlayer;
+        public string
+    }
+}
